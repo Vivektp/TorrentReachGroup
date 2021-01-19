f7618d4e40f628030b472b6c039c3f86a8beb83d
Vivektp <75740863+Vivektp@users.noreply.github.com>
2021-01-19 16:39:26 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,7 @@
+from tobrot.sample_config import Config
+
+class Config(Config):
+  TG_BOT_TOKEN = ""
+  APP_ID = 6
+  API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
+  AUTH_CHANNEL = [-1001234567890]
