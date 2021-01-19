e4b98c8a3789f885bdfeeae358f75ecab1b15655
Vivektp <75740863+Vivektp@users.noreply.github.com>
2021-01-19 16:55:02 +0530

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -1,7 +1,7 @@
 from tobrot.sample_config import Config
 
 class Config(Config):
-  TG_BOT_TOKEN = ""
+  TG_BOT_TOKEN = "1574583566:AAFQBVNe2tukCAZHuDN6n10nws_U_01QvN0"
   APP_ID = 2855902
   API_HASH = "b5ce4eda2c5e136b7ab718623ee335ac"
   AUTH_CHANNEL = [-1001249849587]
